3deb4bcb5ef2a3713aba3684f7ecdda255b19feb
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-27 11:35:44 +0100

    feat: bonus

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ createApp({
     data() {
         return {
             message: 'Hello World!',
+            img: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png',
+            alt: 'Vue Js Logo',
         }
     }
-}).mount('#app')
+}).mount('#app');
